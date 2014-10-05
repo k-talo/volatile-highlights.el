fb2abc2d4d4051a9a6b7c8de2fe7564161f01f24
k-talo <Keitaro.Miyazaki@gmail.com>
2014-10-05 14:40:03 +0900

    - Update version number.

--- volatile-highlights.el
+++ volatile-highlights.el
@@ -9,7 +9,7 @@
 ;; Revision: $Id$
 ;; URL: http://www.emacswiki.org/emacs/download/volatile-highlights.el
 ;; GitHub: http://github.com/k-talo/volatile-highlights.el
-;; Version: 1.10
+;; Version: 1.11
 ;; Contributed by: Ryan Thompson and Le Wang.
 
 ;; This file is not part of GNU Emacs.
